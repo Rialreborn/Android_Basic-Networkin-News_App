891d442ea9c55da406938171a0b60cfa0fb9c0f5
Zane Rastrick <zanerast@gmail.com>
2017-12-10 15:21:16 +0000

    Add intent to news URL

--- app/src/main/java/com/example/android/mynewsapp/MainActivity.java
+++ app/src/main/java/com/example/android/mynewsapp/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.android.mynewsapp;
 
 import android.app.LoaderManager;
+import android.content.Intent;
 import android.content.Loader;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -46,8 +50,25 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             View progressBar = (View) findViewById(R.id.progress_bar);
             progressBar.setVisibility(View.GONE);
             mEmptyStateTextView.setText(R.string.network_issues);
+            ImageView imageView = (ImageView) findViewById(R.id.empty_state_image);
+            imageView.setVisibility(View.VISIBLE);
         }
 
+        newsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                NewsObject currentNewsObject = adapter.getItem(position);
+
+                Uri newsUri = Uri.parse((currentNewsObject.getNewsUrl()));
+
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, newsUri);
+
+                startActivity(websiteIntent);
+            }
+        });
+
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_state);
+        newsListView.setEmptyView(mEmptyStateTextView);
 
     }
 
@@ -59,6 +80,10 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public void onLoadFinished(Loader<List<NewsObject>> loader, List<NewsObject> data) {
+        View progressbar = (View) findViewById(R.id.progress_bar);
+        progressbar.setVisibility(View.GONE);
+
+        mEmptyStateTextView.setText(R.string.no_news_found);
         adapter.clear();
 
         if (data != null && !data.isEmpty()) {
